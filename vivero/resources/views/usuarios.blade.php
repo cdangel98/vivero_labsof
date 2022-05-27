4dc7f9c44e0020a7df907fe71fd5e41b8666d294
YeimiHernandez <42257457+YeimiHernandez@users.noreply.github.com>
2022-05-27 16:37:43 -0500

    HU-2 Add file base to view
    Se agrego archivos base para la vista de usuario a navegación principal

---
+++ vivero/resources/views/usuarios.blade.php
@@ -0,0 +1,9 @@
+@extends("template/layout")
+
+@section('scripts')
+    <script src="{{asset('js/usuarios.js')}}"></script>    
+@endsection
+
+@section('content')
+    <h1>Contenido Usuarios</h1>
+@endsection
