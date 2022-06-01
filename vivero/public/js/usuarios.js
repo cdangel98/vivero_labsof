e119d3035870dd6670dc5a92307a6833bbd60f03
cdangel98 <89823920+cdangel98@users.noreply.github.com>
2022-06-01 15:54:38 -0500

    HU-2 Update usuarios.js
    Se agrego estructura básica de vuejs para crear y listar usuarios

--- vivero/public/js/usuarios.js
+++ vivero/public/js/usuarios.js
@@ -1,1 +1,24 @@
-console.log('usuarios')
+new Vue({
+    el: '#app',
+    delimiters: ['${', '}'],
+    data: () => ({
+        newUser: {
+            document: '',
+            name: '',
+            rol: '',
+        },
+        listUsers: []
+    }),
+    mounted() {
+        this.searchUsers()
+    },
+    methods: {
+        saveUser() {
+            console.log(this.newUser)
+        },
+
+        searchUsers() {
+            console.log(this.listUsers)
+        }
+    }
+})
