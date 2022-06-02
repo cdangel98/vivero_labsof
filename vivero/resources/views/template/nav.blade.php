a35f69ca4dfe63741e01381eba42842d549cdd3f
Carlos Daniel Angel Castaño <ingenierodesarrollo2@comersantander.local>
2022-06-02 11:22:56 -0500

    HU-2 diseño de interfaz de usuario

--- vivero/resources/views/template/nav.blade.php
+++ vivero/resources/views/template/nav.blade.php
@@ -1,6 +1,18 @@
-<nav>
-    <ul>
-        <li><a href="/usuarios">Usuarios</a></li>
-        <li><a href="/viveros">Viveros</a></li>
-    </ul>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-3">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="">Vivero</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarScroll">
+            <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
+                <li class="nav-item">
+                    <a class="nav-link" href="/usuarios">Usuarios</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/viveros">Viveros</a>
+                </li>
+            </ul>
+        </div>
+    </div>
 </nav>
